9010127fd156794eeff09686767cc62692a5e682
jojox110 <jonathansamuel.leger@gmail.com>
2024-03-12 10:19:27 -0300

    basic TS backend Vue frontend works with ts-node

--- StudyTracker/src/views/HomeView.vue
+++ StudyTracker/src/views/HomeView.vue
@@ -1,9 +1,31 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div class="grid">
+      <div class="one">One</div>
+      <div class="two">Two</div>
+      <div class="three">Three</div>
+    </div>
   </main>
 </template>
+
+<script>
+export default {
+  name: "get-request-set-headers",
+  data() {
+    return {
+      totalVuePackages: null
+    };
+  },
+  created() {
+    // GET request using fetch with set headers
+    const headers = { "Content-Type": "application/json" };
+    fetch("https://api.npms.io/v2/search?q=vue", { headers })
+      .then(response => response.json())
+      .then(data => (console.log(data)));
+  }
+};
+</script>
+
+<style>
+@import 'styles/test.css';
+</style>
